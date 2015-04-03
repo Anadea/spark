2e219e8f9bf45fbe49f9d6ac3431deacb8c405b9
Dmitriy Kiriyenko <dmitriy.kiriyenko@anahoret.com>
2015-04-03 16:37:06 +0300

    More clear variable name.

--- templates/config/initializers/mail_interceptor.rb
+++ templates/config/initializers/mail_interceptor.rb
@@ -1,3 +1,3 @@
 Mail.register_interceptor(
-  RecipientInterceptor.new(ENV["ALLOWED_EMAIL_RECIPIENTS"])
-) if ENV.key?("ALLOWED_EMAIL_RECIPIENTS")
+  RecipientInterceptor.new(ENV["EMAIL_RECIPIENTS"])
+) if ENV.key?("EMAIL_RECIPIENTS")
